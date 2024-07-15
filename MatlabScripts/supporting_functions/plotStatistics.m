86fb04449fc3b304d2573a7e5034246d03606924
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-07-15 13:43:22 +0200

    Update plotStatistics.m

--- MatlabScripts/supporting_functions/plotStatistics.m
+++ MatlabScripts/supporting_functions/plotStatistics.m
@@ -1,7 +1,12 @@
 function plotStatistics(p, max, xedgemin, xedgemax)
 
-xvalue = (xedgemin+xedgemax)/2;
-plot([xedgemin xedgemax],[max*1.05 max*1.05], 'Marker', '|', 'Color', 'k')
+if isempty(xedgemax)
+    xvalue = xedgemin;
+else
+    xvalue = (xedgemin+xedgemax)/2;
+    plot([xedgemin xedgemax],[max*1.05 max*1.05], 'Marker', '|', 'Color', 'k')
+end
+
 if p <= 0.05 && p > 0.01
     text(xvalue, max*1.1,'*','HorizontalAlignment','center','VerticalAlignment','top')
 elseif p <= 0.01 && p > 0.001
