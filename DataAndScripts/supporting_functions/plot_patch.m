db8d33d0d57a519d768f6a23208594334eaaf784
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2025-03-17 11:37:57 +0100

    Update plot_patch.m

--- DataAndScripts/supporting_functions/plot_patch.m
+++ DataAndScripts/supporting_functions/plot_patch.m
@@ -8,10 +8,11 @@ inter_data = cat(1, inter_data{:});
 if ~exist('figgi','var') || isempty(figgi)
     fig = figure;
 else
-    fig = figgi; hold on
+    figure(figgi)
+    fig = figgi;
 end
-plot(common_axis, mean(inter_data, 1, "omitnan"), "Color", color)
-ylim([0,1]); hold on;
+plot(common_axis, mean(inter_data, 1, "omitnan"), "Color", color); hold on
+ylim([0,1])
 sig_plot = std(inter_data,1,1,'omitnan');
 mu_plot = mean(inter_data,1,"omitnan");
 my_patch = patch('XData', [common_axis, flip(common_axis)],'YData', [sig_plot+mu_plot, flip(-sig_plot+mu_plot)]);
